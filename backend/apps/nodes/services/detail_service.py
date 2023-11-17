435a0363ee916d9379755a3680f56c7a3c884bd6
hvoking <gustavoinge@hotmail.com>
2023-11-17 15:02:30 -0300

    backup 17-11-23

---
+++ backend/apps/nodes/services/detail_service.py
@@ -0,0 +1,22 @@
+# App imports
+from apps.nodes.services.patterns import nodeDetailPattern, nodeInputPattern, nodeInputWordsPattern
+
+# Utils imports
+from apps.utils.paths import threejsTypesPath, threeExportationFilePath
+
+def readFile(path):
+	with open(path, 'r', encoding = 'utf8') as f:
+		text = f.read()
+	return text
+	
+# File that contains all nodes available for search
+text = readFile(threeExportationFilePath)
+
+def get_detail(node):
+	nodePath = nodeDetailPattern(node, text)
+	nodePath = threejsTypesPath + nodePath[0].replace("'", "").replace('.', '') + ".d.ts"
+	nodeInfo = readFile(nodePath)
+	nodeInput = nodeInputPattern(nodeInfo)
+	nodeInputWords = nodeInputWordsPattern(nodeInput)
+	resp = {"inputs": nodeInputWords, "output": node}
+	return resp
--- backend/apps/nodes/views/detail_view.py
+++ backend/apps/nodes/views/detail_view.py
@@ -1,27 +1,11 @@
 # App imports
-from apps.nodes.services.patterns import nodeDetailPattern, nodeInputPattern, nodeInputWordsPattern
-
-# Utils imports
-from apps.utils.paths import threejsTypesPath, threeExportationFilePath
-
+from apps.nodes.services import detail_service
 # Third-party imports
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-def readFile(path):
-	with open(path, 'r', encoding = 'utf8') as f:
-		text = f.read()
-	return text
-	
-# File that contains all nodes available for search
-text = readFile(threeExportationFilePath)
-
 # Single node matchs request
 @api_view(['GET'])
 def nodesDetail(request, node):
-	nodePath = nodeDetailPattern(node, text)
-	nodePath = threejsTypesPath + nodePath[0].replace("'", "").replace('.', '') + ".d.ts"
-	nodeInfo = readFile(nodePath)
-	nodeInput = nodeInputPattern(nodeInfo)
-	nodeInputWords = nodeInputWordsPattern(nodeInput)
-	return Response({"inputs": nodeInputWords, "output": node})
+	resp = detail_service.get_details(node)
+	return Response(resp)
